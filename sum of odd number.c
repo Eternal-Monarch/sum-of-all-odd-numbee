c29d7011a443d17988a937f004212c84070cf425
SubhasishDutta-Coder <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-03-26 01:46:02 +0600

    Add files via upload

---
+++ sum of odd number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,n,i;
+//printf("enter the last digit of sequal\n");
+//scanf("%d",&n);
+for(i=1;i<=10;i++);
+printf("the total summation of sequal is %d\n",i);
+return 0;
+}
